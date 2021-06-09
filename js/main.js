5aef8d7c63df7adb49ff76cbfd6055b2880b2f25
easternleaves <idmitrydsgn@gmail.com>
2021-06-10 00:58:43 +0300

    Добавляет проверки во вторую функцию

--- js/main.js
+++ js/main.js
@@ -19,12 +19,23 @@ function getRandomIntInclusive (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;  // max - min + 1: кол-во элементов диапазона [min, max], +1 мы добавляем чтобы включить максимальное значение?
 }
 
-getRandomIntInclusive();
-
 // 2. Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
 
 function getRandomFloatingPoint (min, max, digits) {
+  if (min < 0 || max < 0) {
+    throw new Error('Все значения должны быть положительными');
+  }
+
+  if (max < min) {
+    throw new Error('Конечное значение не может быть меньше начального');
+  }
+
+  if (max === min) {
+    throw new Error('Начальное и конечное значения не должны совпадать');
+  }
+
   return (Math.random() * (max - min + 1) + min).toFixed(digits);
 }
 
+getRandomIntInclusive();
 getRandomFloatingPoint();
